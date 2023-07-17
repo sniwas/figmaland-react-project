d49476ddddc38c06bdbefcddc93fb38e80acf01e
s-niwas <niwas.s@increscotech.com>
2023-07-17 16:13:46 +0530

    test commit

--- src/Components/Gallery.js
+++ src/Components/Gallery.js
@@ -15,7 +15,7 @@ export default function Gallery() {
         </div>
       </div>
 
-      <div className="max-w-6xl mx-auto px-2 sm:px-2 lg:px-1 py-2">
+      <div className="max-w-6xl mx-auto px-2 sm:px-2 lg:px-1 lg:py-2">
         <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-4">
           <div className="w-fit rounded-lg sahdow-lg overflow-hidden flex flex-col justify-center items-center">
             <div>
